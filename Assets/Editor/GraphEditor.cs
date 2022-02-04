ad8530f7ec1434fe2b93994709721f6adc22f7b8
seanmul10 <36199832+smuldoon1@users.noreply.github.com>
2022-02-04 08:45:42 +0000

    Fixed background texture
    Background texture now matches the position of UI elements by drawing the texture from the Rect origin rather than (0,0)
    
    Slightly decreased contrast of grid texture.

--- Assets/Editor/GraphEditor.cs
+++ Assets/Editor/GraphEditor.cs
@@ -23,14 +23,18 @@ public class GraphEditor : EditorWindow
         if (window == null)
             window = GetWindow<GraphEditor>();
         Rect graphView = new Rect(window.position.width * 0.2f, 0f, window.position.width * 0.8f, window.position.height);
-        Texture gridTexture = AssetDatabase.LoadAssetAtPath<Texture>("Assets/grid.png");
-        GUI.DrawTextureWithTexCoords(new Rect(graphView.x, graphView.y, graphView.width, graphView.height), gridTexture, new Rect(0, 0, graphView.width / gridTexture.width, graphView.height / gridTexture.height));
+        Texture backgroundTexture = AssetDatabase.LoadAssetAtPath<Texture>("Assets/grid.png");
+        GUI.DrawTextureWithTexCoords(new Rect(graphView.x, graphView.height, graphView.width, -graphView.height), backgroundTexture, new Rect(0, 0, graphView.width / backgroundTexture.width, graphView.height / backgroundTexture.height));
         GUI.BeginGroup(graphView);
         if (graph == null)
             graph = new Graph();
         graph.Render();
         if (new Rect(0, 0, window.position.width * 0.8f, window.position.height).Contains(Event.current.mousePosition))
             graph.Input();
+        GUI.EndGroup();
+        Rect settingsView = new Rect(0f, 0f, window.position.width * 0.2f, window.position.height);
+        GUI.BeginGroup(settingsView);
+        EditorGUILayout.TextField("");
         GUI.EndGroup();
     }
 }
